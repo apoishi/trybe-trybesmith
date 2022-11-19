cf1a2fea4cc24b3796c29000a64759aaf846a353
Ariane Padilha <ariane.padilha@outlook.com.br>
2022-11-19 00:55:17 -0300

    ✨ Req 07 created validations for users

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -2,12 +2,15 @@
 import { IUser } from '../interfaces';
 import UserModel from '../models/user.model';
 import generateToken from '../utils/jwt';
+import validateSchema from '../validations/validateSchema';
+import { userSchema } from '../validations/schema';
 
 // Requirement 03
 export default class UsersService {
   public user = new UserModel();
 
   public async create(userData: IUser) {
+    await validateSchema(userSchema, userData);
     await this.user.create(userData);
     const token = generateToken(userData);
     return token;
--- src/validations/schema.ts
+++ src/validations/schema.ts
@@ -10,4 +10,11 @@ const productSchema: Joi.Schema = Joi.object({
   amount: Joi.string().min(3).required(),
 });
 
-export { loginSchema, productSchema };
+const userSchema: Joi.Schema = Joi.object({
+  username: Joi.string().min(3).required(),
+  classe: Joi.string().min(3).required(),
+  level: Joi.number().integer().min(1).required(),
+  password: Joi.string().min(8).required(),
+});
+
+export { loginSchema, productSchema, userSchema };
